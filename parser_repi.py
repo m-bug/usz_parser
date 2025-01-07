5eda5ae73d305af035b8165590aaaa3dac79b6d9
zabcz <marc.bugmann@outlook.com>
2025-01-07 23:33:07 +0100

    reset sanitize links in parser_repi

--- parser_repi.py
+++ parser_repi.py
@@ -5,11 +5,11 @@ import re
 
 def extract_filtered_links(url, pattern):
     try:
-        # call webside
+        # call website
         response = requests.get(url)
         response.raise_for_status()
 
-        # pars content
+        # parse content
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # collect and filter links
@@ -18,10 +18,7 @@ def extract_filtered_links(url, pattern):
             absolute_link = urljoin(url, a_tag['href'])
             # filter by pattern
             if re.search(pattern, absolute_link):
-                # Remove any letter after the last number before ".html", e.g r_xi_11b.html => r_xi_11.html
-                sanitized_link = re.sub(r'(\d)[a-zA-Z]+(?=\.html)', r'\1', absolute_link)
-                # debug: print(sanitized_link)
-                links.append(sanitized_link)
+                links.append(absolute_link)
 
         return links
 
@@ -36,7 +33,7 @@ url = "https://histodb11.usz.ch/pages/rep_inhalt.html"
 pattern = r"/r_"
 filtered_links = extract_filtered_links(url, pattern)
 
-
-with open("repi_links.txt", "a") as f:
+# Open the file in write mode to empty it before writing
+with open("repi_links.txt", "w") as f:
     for link in filtered_links:
         f.write(link+"\n")
